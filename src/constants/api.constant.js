7c1ff9e2564392682e39f95066a2cace23fc102a
vthn303 <vthn303@gmail.com>
2024-12-19 14:24:42 +0700

    ✨feature: define some constant and localStorage

--- src/constants/api.constant.js
+++ src/constants/api.constant.js
@@ -1,1 +1,43 @@
-import axios from "axios";
+export const ApiConstant = {
+    auth:{
+        login: '/auths/login'
+    },
+    users:{
+        getAllUser: '/users',
+        currentUser: '/users/me',
+        createUser: '/users',
+        updateUser: '/users'
+    },
+    order:{
+        createOrder: '/orders',
+        getOrderByCurrentUser: '/orders',
+    },
+    products:{
+        getAllProduct: '/products',
+        getProductByProductId: '/products/',
+        createProduct: '/products',
+        deleteProductByProductId: '/products/',
+        updateProductByProductId: '/products/',
+
+    },
+    category:{
+        createCategory: '/categories',
+        getAllCategory: '/categories',
+        deleleCategoryByCategoryId: '/categories/',
+        updateCategoryByCategoryId: '/categories/',
+        getSubCategoryByCategoryId: '/categories/',
+    },
+    cart:{
+        getAllCart: '/carts',
+        createCart: '/carts',
+        deleteCartByCartItemId: '/carts/',
+        updateCartByCartItemId: '/carts/',
+        getProductByCartItemId: '/carts/',
+    },
+    payment:{
+        createPayment: '/payments',
+        updatePaymentByPaymentId: '/payments/',
+    },
+
+    
+}
