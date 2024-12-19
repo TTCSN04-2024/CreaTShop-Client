13f46743ad2673bbd3e61fbd28b820de7ea620ed
vthn303 <vthn303@gmail.com>
2024-12-19 14:25:48 +0700

    ✨feature: main layout for user

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,30 +1,29 @@
-import Header from '../common/Header'
-import { Outlet } from 'react-router-dom'
-import Footer from '../common/Footer'
-import { Toaster } from 'react-hot-toast';
+import Header from '../common/Header';
+import {Outlet} from 'react-router-dom';
+import Footer from '../common/Footer';
+import {Toaster} from 'react-hot-toast';
 const MainLayout = () => {
-  return (
-    <div>
-    <Toaster
-        position="top-center"
-        toastOptions={{
-          style: {
-            padding: '16px',
-            color: '#333',
-          },
-        }}
-      />
-      <Header />
-      <div className="container">
-        <div className="row">
-          <div className="">
-            <Outlet/>
-          </div>
-        </div>
-      </div>
-      <Footer/>
-    </div>
-  )
-}
+	return (
+		<div>
+			<Toaster
+				position='top-left'
+				reverseOrder={false}
+				toastOptions={{
+					duration: 980,
+				}}
+				limit={10}
+			/>
+			<Header />
+			<div className='container'>
+				<div className='row'>
+					<div className='flex-grow'>
+						<Outlet />
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
+};
 
-export default MainLayout
+export default MainLayout;
