5a74553df3ae1c98c4e448afb8da36934f515662
vthn303 <vthn303@gmail.com>
2024-12-19 14:27:02 +0700

    ✨feature: dashboard admin

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,25 @@
+import { Outlet } from "react-router-dom"
+import SidebarAdmin from "../components/SideBarAdmin/SidebarAdmin"
 
 const Dashboard = () => {
+  
   return (
-    <div>
-        <aside>
-            <p>hello</p>
-        </aside>
+    <div className="container-fluid">
+      <div className="border border-black p-5 flex gap-10 justify-center">
+        <div className="p-28 border border-red-600">
+          <p>Tổng người dùng </p>
+          <p>4</p>
+        </div>
+        <div className="p-28 border border-red-600">
+          <p>Tổng sản phẩm</p>
+          <p>4</p>
+        </div>
+        <div className="p-28 border border-red-600">
+          <p>Tổng đơn đặt hàng</p>
+          <p>4</p>
+        </div>
+      </div>
+       
     </div>
   )
 }
